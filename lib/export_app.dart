b63f0bafb1ecde2e2852ee3e98218b34ad35d25d
tushal kaklotar <tushalkaklotar90@gmail.com>
2023-12-22 12:32:52 +0530

    KT || 22/12/2023

---
+++ lib/export_app.dart
@@ -0,0 +1,5 @@
+// firebase
+export 'firebase_options.dart';
+export 'package:flutter/cupertino.dart';
+export 'package:firebase_core/firebase_core.dart';
+
---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'export_app.dart';
+
+ void main () async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
